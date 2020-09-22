8b701e93e9a26eacabfd21425821eddbdfe99424
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2020-09-22 23:08:21 +0300

    add tests for NewPublisher

--- publisher.go
+++ publisher.go
@@ -14,35 +14,52 @@ func NewPublisher(
 	connCloseCh := make(chan *amqp.Error, 1)
 
 	p := publisher.New(connCh, connCloseCh, opts...)
-
-	go func() {
-		defer close(connCh)
-		defer close(connCloseCh)
-
-		for {
-			select {
-			case conn, ok := <-amqpConnCh:
-				if !ok {
-					return
-				}
-
-				select {
-				case connCh <- &publisher.AMQP{Conn: conn}:
-				case <-p.Closed():
-					return
-				}
-
-				select {
-				case err := <-amqpConnCloseCh:
-					connCloseCh <- err
-				case <-p.Closed():
-					return
-				}
-			case <-p.Closed():
-				return
-			}
-		}
-	}()
+	go proxyPublisherConn(amqpConnCh, amqpConnCloseCh, connCh, connCloseCh, p.Closed())
 
 	return p
+}
+
+func proxyPublisherConn(
+	amqpConnCh <-chan *amqp.Connection,
+	amqpConnCloseCh <-chan *amqp.Error,
+	connCh chan publisher.Connection,
+	connCloseCh chan *amqp.Error,
+	publisherCloseCh <-chan struct{},
+) {
+	defer close(connCloseCh)
+	defer close(connCh)
+
+	for {
+		select {
+		case conn, ok := <-amqpConnCh:
+			if !ok {
+				return
+			}
+
+			select {
+			case connCh <- &publisher.AMQP{Conn: conn}:
+			case err := <-amqpConnCloseCh:
+				select {
+				case connCloseCh <- err:
+				default:
+				}
+
+				continue
+			case <-publisherCloseCh:
+				return
+			}
+
+			select {
+			case err := <-amqpConnCloseCh:
+				select {
+				case connCloseCh <- err:
+				default:
+				}
+			case <-publisherCloseCh:
+				return
+			}
+		case <-publisherCloseCh:
+			return
+		}
+	}
 }
