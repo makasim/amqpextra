14be4cd392a4c3b3f308b208670e5d33d6ec2d1c
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2020-09-24 23:18:53 +0300

    Move declare helpers to own module

---
+++ declare/declare.go
@@ -0,0 +1,64 @@
+package declare
+
+import (
+	"context"
+
+	"log"
+
+	"github.com/streadway/amqp"
+	"github.com/makasim/amqpextra"
+)
+
+func DeclareTempQueue(
+	ctx context.Context,
+	c *amqpextra.Connection,
+) (amqp.Queue, error) {
+	return DeclareQueue(
+		ctx,
+		c,
+		"",
+		false,
+		true,
+		true,
+		false,
+		amqp.Table{},
+	)
+}
+
+func DeclareQueue(
+	ctx context.Context,
+	c *amqpextra.Connection,
+	name string,
+	durable,
+	autDelete,
+	exclusive,
+	noWait bool,
+	args amqp.Table,
+) (amqp.Queue, error) {
+	select {
+	case <-ctx.Done():
+		return amqp.Queue{}, ctx.Err()
+	case <-c.Ready():
+		conn, err := c.Conn()
+		if err != nil {
+			return amqp.Queue{}, err
+		}
+
+		ch, err := conn.Channel()
+		if err != nil {
+			return amqp.Queue{}, err
+		}
+		defer func() {
+			if closeErr := ch.Close(); closeErr != nil {
+				log.Print("amqpextra: declare queue: ch close: %w", closeErr)
+			}
+		}()
+
+		q, err := ch.QueueDeclare(name, durable, autDelete, exclusive, noWait, args)
+		if err != nil {
+			return amqp.Queue{}, err
+		}
+
+		return q, nil
+	}
+}
